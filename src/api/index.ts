6e7bc3118417ddf3eecf5e44e87b6ed89cccb711
NyanRus <68762426+nyanrus@users.noreply.github.com>
2025-03-29 01:07:53 +0900

    test_drizzle

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,14 +2,30 @@ import { app } from '#lib/app'
 import express from 'express'
 import expressWs from 'express-ws'
 
+import { DatabaseSync } from 'node:sqlite';
+
+const database = new DatabaseSync('database.sqlite3');
+
+database.exec(`
+    CREATE TABLE data(
+    key INTEGER PRIMARY KEY,
+  ) STRICT
+`)
+
+const insert = database.prepare('INSERT INTO data (key) VALUES (?)');
+
 expressWs(app)
 
 const router = express.Router()
 
+let num = 0;
+
 router.ws('/', (ws, req) => {
     ws.send("Server Connected.");
     ws.on('message', msg => {
-        console.log(msg)
+        //console.log(`${msg}${num}`);
+        num += 1;
+        insert.run(num)
     })
 })
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,52 @@
-import app from './app.ts'
+// import app from './app.ts'
 
-const port = process.env.PORT || 80
+// const port = process.env.PORT || 80
 
-app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Listening on http://localhost:${port}`)
+// })
+
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/mysql2';
+import { eq } from 'drizzle-orm';
+import { usersTable } from './db/schema';
+  
+const db = drizzle(process.env.DATABASE_URL!);
+
+async function main() {
+await db.delete(usersTable)
+  const user: typeof usersTable.$inferInsert = {
+    name: 'John',
+    email: 'john@example.com',
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log('New user created!')
+
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users)
+  /*
+  const users: {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }[]
+  */
+
+  await db
+    .update(usersTable)
+    .set({
+      email: "example@example.com"
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log('User info updated!')
+
+  const useras = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', useras)
+
+  await db.delete(usersTable).where(eq(usersTable.email, user.email));
+  console.log('User deleted!')
+}
+
+main();
